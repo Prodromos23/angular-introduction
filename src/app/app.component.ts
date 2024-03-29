598cdc373cb6d5e76ba5ca1352c1cbcba7f0c576
Pro <prodromos23@windowslive.com>
2024-03-29 22:54:22 +0100

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,17 @@ import { Component } from '@angular/core';
 import { PersonTableComponent } from './components/person-table/person-table.component';
 import { Person } from './shared/interfaces/person';
 import { EventBindExampleComponent } from './components/event-bind-example/event-bind-example.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PersonTableComponent,EventBindExampleComponent],
+  imports: [RouterLink,RouterOutlet,PersonTableComponent,EventBindExampleComponent,WelcomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  name = 'Prodromos';
   // name: string = 'Prodromos';
 
   person0 ={
