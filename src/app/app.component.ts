355e7f912f461ea3238b5d50f65111fcb561d124
Pro <prodromos23@windowslive.com>
2024-03-29 17:21:59 +0100

    person component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,16 @@
 import { Component } from '@angular/core';
+import { PersonTableComponent } from './components/person-table/person-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [PersonTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   name = 'Prodromos';
   // name: string = 'Prodromos';
-  person = {
-    givenName: 'Prodromos',
-    surname: 'Thomaidis',
-    age: 0x19,
-    email: 'prodromos@aueb.gr'
-  }
+
 }
 
