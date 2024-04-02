c6608889c70be12fdc70137e1699f353c40da897
Pro <prodromos23@windowslive.com>
2024-04-03 01:40:06 +0200

    menu-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,25 +4,18 @@ import { Person } from './shared/interfaces/person';
 import { EventBindExampleComponent } from './components/event-bind-example/event-bind-example.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { ListGroupMenuComponent } from './components/list-group-menu/list-group-menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   // RouterLink and RouterOutlet give us the opportunity to use the <routerLink> in the app.component.html
-  imports: [RouterLink,RouterOutlet,PersonTableComponent,EventBindExampleComponent,WelcomeComponent],
+  imports: [RouterLink,RouterOutlet,ListGroupMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   // name: string = 'Prodromos';
-
-
-
-
-
-
-
-
 
 }
 
