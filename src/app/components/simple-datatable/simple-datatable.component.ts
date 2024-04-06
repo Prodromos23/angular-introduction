09229651f7b8dda789d79550605da1345c066ea0
Pro <prodromos23@windowslive.com>
2024-04-06 22:52:04 +0200

    sorting

--- src/app/components/simple-datatable/simple-datatable.component.ts
+++ src/app/components/simple-datatable/simple-datatable.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { sortBy } from 'lodash-es';
 import { EPerson } from 'src/app/shared/interfaces/person';
 
 @Component({
@@ -10,4 +11,38 @@ import { EPerson } from 'src/app/shared/interfaces/person';
 })
 export class SimpleDatatableComponent {
 @Input() data: EPerson[];
+
+  sortOrder = {
+    givenName: 'none',
+    surName: 'none',
+    age: 'none',
+    email: 'none',
+    education : 'none'
+  }
+
+  sortData(sortKey: string) {
+    if (this.sortOrder[sortKey] === 'asc') {
+      this.sortOrder[sortKey] = 'desc';
+      this.data = sortBy(this.data, [sortKey]).reverse();
+  } else {
+      this.sortOrder[sortKey] = 'asc';
+      this.data = sortBy(this.data, [sortKey]);
+    }
+    // for every key in the sort order then set it to 'none'
+    for (let key in this.sortOrder) {
+      if (key !== sortKey) {
+        this.sortOrder[key] = 'none';
+      }
+    }
+  }
+  sortSign(sortKey: string) {
+    if (this.sortOrder[sortKey] === 'asc') {
+      return '▲';
+    } else if (this.sortOrder[sortKey] === 'desc') {
+      return '▼';
+    }else {
+      return '';
+    }
+  }
+
 }
